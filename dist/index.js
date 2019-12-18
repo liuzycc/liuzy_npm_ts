214e0ca13d9453361d133eedc686f2208dd9cfe0
liuziyi <liuzy8@ziroom.com>
2019-12-18 17:07:50 +0800

    1

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.demoFn = function (str) {
+    return "\u5165\u53C2" + str + "####\u6210\u529F\u8C03\u7528";
+};
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+export const demoFn = (str: string) => {
+  return `入参${str}####成功调用`;
+};
